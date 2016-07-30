fb4f4c87fd93bec6d0f6f1dad7ad64bc33cccc1c
mattthousand <matt.buckley@raizlabs.com>
2016-07-30 17:07:03 -0400

    Add interactive transitions example code.

---
+++ 2016-01-09-interactive-transitions/InteractiveTransitionExample/README.md
@@ -0,0 +1,11 @@
+# `UIViewControllerInteractiveTransitioning` Example
+
+<p align="center" >
+<br/>
+<img src="https://raw.github.com/mattthousand/InteractiveTransitionExample/master/transition.gif" alt="Overview" />
+<br/>
+</p>
+
+This is a small example app written to accompany a blog post (http://www.matt-buckley.com/interactive-transitions/) that demonstrates how to use the protocol to
+implement interactive transitions between view controllers.
+
---
+++ 2016-08-01-quaternions/Quaternion Example/Assets/RotationScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+
+	public float speed = 300.0f;	
+	private float time = 0.0f;
+	// Update is called once per frame
+	void Update () {	
+		// Quaternion q = Quaternion.AngleAxis(Time.deltaTime * 10f, Vector3.up);
+
+		// Quaternion e = Quaternion.Euler(0,30,0);
+
+		// transform.rotation = Quaternion.Lerp(transform.rotation, e, Time.deltaTime);
+
+		time = Time.time;
+		Quaternion circleAroundZAxis = Quaternion.AngleAxis(10 * time, Vector3.up);
+
+		Debug.Log("Time:" + time);
+		Debug.Log("Current Rotation: " + transform.rotation);
+		Debug.Log("New Rotation: " + circleAroundZAxis);
+		Debug.Log("Identity: " + Quaternion.identity);
+		transform.rotation = Quaternion.Lerp(transform.rotation, circleAroundZAxis, speed);
+	}
+	
+}
